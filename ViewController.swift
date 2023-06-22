5b6198535496b10c940d6040e04669cf32d8a87d
Igor Pereira Dourado <70119242+IgorDouradoBR@users.noreply.github.com>
2023-06-22 14:54:00 -0300

    Favorites

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music
+//
+//  Created by Igor Dourado  on 19/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
